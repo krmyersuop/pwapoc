c7bddccc48f67ac1f8f786126bbfcc0cfe3513a3
krmyersuop <50753979+krmyersuop@users.noreply.github.com>
2019-05-17 15:23:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded",function(){
 function renderToDo() {
 
     if ('caches' in window) {
-      const url = "${window.location.origin}/apis/todo.json";
+      const url = window.location.origin + '/apis/todo.json';
       caches.match(url)
         .then((response) => {
             if (response) {
