0f9f47ebda28e0ec5def449c8315b598e2721165
krmyersuop <50753979+krmyersuop@users.noreply.github.com>
2019-05-20 17:59:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -34,21 +34,23 @@ function renderToDo(response) {
   if (response) {
     const now = new Date();
     const responseTime = new Date(JSON.parse(response.headers.get("x-responseTime")));
-    const tasklist = response.json();
-    const html = `
-    <ul class="task-list">
-    <li>This is from CACHE!</li>
-    ${tasklist.map(task => 
-          `<li class="list-item">
-          <div class="due">
-              <div class="due-label">Due</div>
-              <div class="due-date">${task.due}</div>
-          </div>
-          <a href="${task.url}">${task.label}</a>
-          </li>`
-      ).join('')}
-      </ul>
-      `;
-      document.querySelector('#todo').innerHTML = html;
+    response.json()
+      .then(tasklist => {
+        const html = `
+        <ul class="task-list">
+        <li>This is from CACHE!</li>
+        ${tasklist.map(task => 
+              `<li class="list-item">
+              <div class="due">
+                  <div class="due-label">Due</div>
+                  <div class="due-date">${task.due}</div>
+              </div>
+              <a href="${task.url}">${task.label}</a>
+              </li>`
+          ).join('')}
+          </ul>
+          `;
+          document.querySelector('#todo').innerHTML = html;
+      });
   }
 }
